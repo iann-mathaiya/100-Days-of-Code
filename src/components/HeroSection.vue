0bf68fde7827b5f33bccd99b52ea785929cab951
Ian Mathaiya <iannmathaiya@gmail.com>
2023-01-24 05:02:33 +0300

    landing page clean up

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -42,14 +42,9 @@
               </p>
               <div class="mt-8 flex gap-x-4 sm:justify-center">
                 <a href="/about"
-                  class="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
+                  class="inline-block rounded-lg bg-gray-800 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-gray-900">
                   Get started{{ ' ' }}
                   <span class="text-indigo-200" aria-hidden="true">&rarr;</span>
-                </a>
-                <a href="#"
-                  class="inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
-                  Live demo{{ ' ' }}
-                  <span class="text-gray-400" aria-hidden="true">&rarr;</span>
                 </a>
               </div>
             </div>
