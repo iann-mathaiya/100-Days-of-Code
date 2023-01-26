17764b09da1e4b4c0057dc6ef685820bae9dda62
Ian Mathaiya <iannmathaiya@gmail.com>
2023-01-26 05:08:45 +0300

    responsiveness in challenges page

--- src/views/Challenges.vue
+++ src/views/Challenges.vue
@@ -3,15 +3,13 @@
     <main class="m-5 md:m-10">
         <h1 class="font-bold text-gray-900 text-xl lg:text-4xl md:text-3xl sm:text-3xl">100 Challenges for 100 Days</h1>
         <div class="grid grid-cols-2 mt-5 gap-4">
-            <div v-for="(challenge, index) in challenges" :key="index" class="relative rounded-md">
-                <div>
-                    <img src="" alt="" srcset="" />
-                    <img v-if="challenge.completed === true" src="" alt="" srcset="" />
-                </div>
-                <div class="flex justify-end">
-                    <div class="w-fit h-16 p-3 flex items-center gap-1 justify-center bg-gray-600 rounded-md ">
-                        <FileLock2 class="text-gray-100" :size="32" aria-hidden="true" />
-                        <h3 class="text-gray-100 text-3xl">{{ challenge.id }}</h3>
+            <div v-for="(challenge, index) in challenges" :key="index" class="relative rounded-xl">
+                <img :src="challenge.imageBefore" class="rounded-xl" alt="" srcset="" />
+                <img v-if="challenge.is_completed === true" class="rounded-xl" src="" alt="" srcset="" />
+                <div class="absolute bottom-2 right-2 md:bottom-4 md:right-4">
+                    <div class="w-fit h-fit md:h-16 p-2 md:p-3 flex items-center gap-1 justify-center bg-gray-600 rounded-md ">
+                        <FileLock2 class="text-gray-100 h-5 w-5" aria-hidden="true" />
+                        <h3 class="text-gray-100 text-base md:text-3xl">{{ challenge.id }}</h3>
                     </div>
                 </div>
             </div>
@@ -27,54 +25,54 @@ const challenges = [
     {
         id: 1,
         name: "Day One",
-        imageBefore: "",
+        imageBefore: "./src/assets/challenges/placeholder-image.webp",
         imageAfter: "",
-        completed: false,
+        is_completed: false,
         tag: "square",
     },
 
     {
         id: 2,
         name: "Day Two",
-        imageBefore: "",
+        imageBefore: "./src/assets/challenges/placeholder-image.webp",
         imageAfter: "",
-        completed: false,
+        is_completed: false,
         tag: "square"
     },
 
     {
         id: 3,
         name: "Day Three",
-        imageBefore: "",
+        imageBefore: "./src/assets/challenges/placeholder-image.webp",
         imageAfter: "",
-        completed: false,
+        is_completed: false,
         tag: "square",
     },
 
     {
         id: 4,
         name: "Day Four",
-        imageBefore: "",
+        imageBefore: "./src/assets/challenges/placeholder-image.webp",
         imageAfter: "",
-        completed: false,
+        is_completed: false,
         tag: "square"
     },
 
     {
         id: 5,
         name: "Day Five",
-        imageBefore: "",
+        imageBefore: "./src/assets/challenges/placeholder-image.webp",
         imageAfter: "",
-        completed: false,
+        is_completed: false,
         tag: "square",
     },
 
     {
         id: 6,
         name: "Day Six",
-        imageBefore: "",
+        imageBefore: "./src/assets/challenges/placeholder-image.webp",
         imageAfter: "",
-        completed: false,
+        is_completed: false,
         tag: "square"
     }
 ]
